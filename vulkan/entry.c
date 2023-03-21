a6b4f2b89f325eb8db137933a621872898e14281
Nikita Romanyuk <kelvium@yahoo.com>
2023-03-21 20:57:53 +0300

    vulkan: export pRendererEntry for the engine
    This is a legendary commit. Legends will remember it forever.

--- vulkan/entry.c
+++ vulkan/entry.c
@@ -4,4 +4,27 @@
  * Copyright (c) 2023, Nikita Romanyuk
  */
 
+#include <engine/logger.h>
+#include <engine/renderer.h>
 #include <pch/pch.h>
+
+pEngine* gEngine = NULL;
+
+i32 rCreate(pEngine* engine)
+{
+	gEngine = engine;
+
+	pLoggerInfo("rCreate()\n");
+	return 0;
+}
+
+i32 rDestroy(void)
+{
+	pLoggerInfo("rDestroy()\n");
+	return 0;
+}
+
+pRendererEntry gEntry = {
+	.create  = rCreate,
+	.destroy = rDestroy,
+};
