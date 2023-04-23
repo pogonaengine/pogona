9c318733c6193fffaa24465dea3d9ccd8996b909
Nikita Romanyuk <kelvium@yahoo.com>
2023-04-23 14:08:57 +0300

    runner: port to Windows
    This is the beginning of the times of TRUE cross-platformability!

--- runner/main.c
+++ runner/main.c
@@ -6,9 +6,11 @@
 
 #include <../config.h>
 
-#ifndef pOS_UNIX
+/* FIXME: this check should be here
+#if !defined(pOS_UNIX)
 # error go port me!
 #endif
+*/
 
 #if defined(pOS_UNIX) && !defined(pOS_DARWIN)
 # define LIBRARY_NAME "libpogona.so"
@@ -18,10 +20,26 @@
 # define LIBRARY_NAME "libpogona.dll"
 #endif
 
-#include <dlfcn.h>
+#ifdef pOS_UNIX
+# include <dlfcn.h>
+#endif
+#ifdef pOS_WINDOWS
+# include <windows.h>
+#endif
 #include <engine/defines.h>
 #include <engine/engine.h>
 #include <pch/pch.h>
+
+#if defined(pOS_UNIX)
+# define dlopen  dlopen
+# define dlsym   dlsym
+# define dlerror dlerror
+# define dlclose dlclose
+#elif defined(pOS_WINDOWS)
+# define dlopen(x, y) LoadLibrary(x)
+# define dlsym(x, y)  GetProcAddress(x, y)
+# define dlclose(x)   FreeLibrary(x)
+#endif
 
 int main(int argc, char** argv)
 {
@@ -31,13 +49,18 @@ int main(int argc, char** argv)
 
 	engineHandle = dlopen(LIBRARY_NAME, RTLD_NOW);
 	if (!engineHandle) {
+#ifdef pOS_UNIX
 		fprintf(stderr, "%s\n", dlerror());
-		return 1;
+#endif
+    result = 1;
+    goto exit;
 	}
 
 	pEngineEntry = (PFN_pEngineEntry) dlsym(engineHandle, "pEngineEntry");
 	if (!pEngineEntry) {
+#ifdef pOS_UNIX
 		fprintf(stderr, "%s\n", dlerror());
+#endif
 		result = 1;
 		goto exit;
 	}
